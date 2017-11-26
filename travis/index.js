94f3bfe67bdde5c32f168f9a3dbcc585a3bc512b
Mitchell Stewart <mitchellwstewart@gmail.com>
2017-11-25 20:09:41 -0500

    travis-testing

--- travis/index.js
+++ travis/index.js
@@ -15,66 +15,61 @@ router.post('/', (request, response, next) => {
     }
   };
   axios
-    .post(
-      'https://api.travis-ci.org/auth/github',
-      { github_token: token },
+  .post(
+    'https://api.travis-ci.org/auth/github',
+    { github_token: token },
+    config
+  )
+  .then(res => {
+    console.log('Travis response token:', res.data.access_token);
+    return res.data.access_token;
+  })
+  .then(async travisToken => {
+    config.headers.Authorization = `token ${travisToken}`;
+    console.log('auth', config);
+    console.log('waiting for travis to sync');
+    
+    //ORIGINAL
+     axios.get(
+      `https://api.travis-ci.org/repos/${username}/${repo}`,
       config
     )
-    .then(res => {
-      console.log('Travis response token:', res.data.access_token);
-      return res.data.access_token;
+  
+    .then(travRepo => {
+      console.log('travRepo: ', travRepo)
+      return travRepo.data.repo.id
     })
-    .then(async travisToken => {
-      config.headers.Authorization = `token ${travisToken}`;
-      console.log('auth', config);
-      console.log('waiting for travis to sync');
+      //.then(res => res.data.repo.id)
+      .then(repoId => {
+        const data = { hook: { id: repoId, active: true } };
+        return axios.put(`https://api.travis-ci.org/hooks`, data, config);
+      })
+      .then(res => console.log(res.data) || response.status(200).send(res.data))
+      .catch(next);
+    })
+    // let travRepo;
+    // while (!travRepo) {
+    //   console.log('waiting')
+    //   axios.get(
+    //     `https://api.travis-ci.org/repos/${username}/${repo}`,
+    //       config
+    //     ).then(res => {
+    //       travRepo = res
+    //     })
+    //   }
 
-      let travRepo;
-      while (!travRepo) {
-        console.log('waiting')
-        axios.get(
-          `https://api.travis-ci.org/repos/${username}/${repo}`,
-          config
-        ).then(res => {
-          travRepo = res
-        })
-      }
+    //   const repoId = travRepo.data.repo.id;
+    //   console.log('travRepo: ', travRepo, 'repoId: ', repoId);
+    //   const data = { hook: { id: repoId, active: true } };
+    //   if (travRepo) {
+    //      axios.put(`https://api.travis-ci.org/hooks`, data, config)
+    //       .then(res => console.log(res.data) || response.status(200).send(res.data))
+    //       .catch(next);
+    //   }
+    // });
 
-      const repoId = travRepo.data.repo.id;
-      console.log('travRepo: ', travRepo, 'repoId: ', repoId);
-      const data = { hook: { id: repoId, active: true } };
-      if (travRepo) {
-         axios.put(`https://api.travis-ci.org/hooks`, data, config)
-          .then(res => console.log(res.data) || response.status(200).send(res.data))
-          .catch(next);
-      }
-    });
-
-  //ORIGINAL
-  //  axios.get(
-  //   `https://api.travis-ci.org/repos/${username}/${repo}`,
-  //   config
-  // )
-
-  // .then(travRepo => {
-  //   console.log('travRepo: ', travRepo)
-  //   return travRepo.data.repo.id
-  // })
-  //   //.then(res => res.data.repo.id)
-  //   .then(repoId => {
-  //     const data = { hook: { id: repoId, active: true } };
-  //     return axios.put(`https://api.travis-ci.org/hooks`, data, config);
-  //   })
-  //   .then(res => console.log(res.data) || response.status(200).send(res.data))
-  //   .catch(next);
-  // })
 });
 
 module.exports = router;
 
-//   while (!correctRepo.data.repo) {
-//     console.log('current get resolution: ', correctRepo)
-//   }
-//   console.log('correct repo: ');
-//   return correctRepo.data.repo.id;
-// })
+
