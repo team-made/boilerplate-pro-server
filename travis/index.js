d41b370ec491ac2e5ed13a934dc755799aedaf34
Mitchell Stewart <mitchellwstewart@gmail.com>
2017-11-26 15:54:28 -0500

    travis-testing

--- travis/index.js
+++ travis/index.js
@@ -4,7 +4,8 @@ const axios = require('axios');
 // const admin = require('firebase-admin')
 
 router.post('/', (request, response, next) => {
-  console.log('req:', request.body);
+  //console.log('req:', request.body);
+  let travisRepo;
   const { token, username, repo } = request.body;
   const config = {
     headers: {
@@ -24,31 +25,41 @@ router.post('/', (request, response, next) => {
       console.log('Travis response token:', res.data.access_token);
       return res.data.access_token;
     })
-    .then(async travisToken => {
+    .then(travisToken => {
       config.headers.Authorization = `token ${travisToken}`;
       console.log('auth', config);
-      let travRepo;
       console.log('waiting for travis to sync');
-      while (!travRepo) {
-        const correctRepo = await axios.get(
-          `https://api.travis-ci.org/repos/${username}/${repo}`,
-          config
-        );
-        console.log('current get resolution: ', correctRepo)
-        travRepo = correctRepo;
-      }
-      console.log('correct repo: ', travRepo);
-      return travRepo;
-    })
-
-    .then(travRepo => travRepo.id)
-    //.then(res => res.data.repo.id)
-    .then(repoId => {
-      const data = { hook: { id: repoId, active: true } };
-      return axios.put(`https://api.travis-ci.org/hooks`, data, config);
-    })
-    .then(res => console.log(res.data) || response.status(200).send(res.data))
-    .catch(next);
+      let travisCheck = setInterval(() => {
+        axios.get(`https://api.travis-ci.org/repos/${username}`, config)
+        //CHECKING ALL TRAVIS REPOS (5 sec intervals) TO SEE IF NEW REPO HAS BEEN SYNCED YET 
+          .then(res => {
+            travisRepo = res.data.repos.find(travRepo => {
+              console.log('repoName: ',repo,'slice: ', travRepo.slug.split('/')[1]);
+              return repo === travRepo.slug.split('/')[1];
+            }); //Sets 'travisRepo' variable to new repo (to confirm that travis synced)
+            if (travisRepo) {
+              clearInterval(travisCheck);
+              console.log('Repo Synced: ', travisRepo);
+              axios.get( //ONLY MAKE THIS CALL WHEN REPO EXISTS
+                  `https://api.travis-ci.org/repos/${username}/${repo}`,
+                  config
+                )
+                // .then(travRepo => {
+                //   return travRepo.data.repo.id;
+                // })
+                .then(travRepo => { 
+                  const data = { hook: { id: travRepo.data.repo.id, active: true } };
+                  return axios.put(`https://api.travis-ci.org/hooks`,data,config);
+                  //Makes put request to travis for engage/active hook
+                })
+                .then(res => console.log(res.data) || response.status(200).send(res.data)
+                )
+                .catch(next);
+            }
+          })
+        
+      }, 5000);
+    });
 });
 
 module.exports = router;
