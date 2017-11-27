bc7d47f0a279faaf25fe64980842970c7d7db334
Evans Stepanov <evans@stepanov.email>
2017-11-27 18:18:48 -0500

    bug(heroku clone): needs user and to remove old repos

--- github/localClone.js
+++ github/localClone.js
@@ -22,6 +22,8 @@ class Cloner {
     }
     this.gitHubAPI = 'https://api.github.com'
     this.status = 'initializing'
+    this.FULL_NAME = 'BOILER PLATE PRO'
+    this.FULL_EMAIL = 'BOILERPLATEGODS@BOILERPLATE.PRO'
     this.destinationRepo = destRepo
     this.destinationUser = destUser
     this.userToken = userToken
@@ -106,9 +108,14 @@ class Cloner {
           return fs.ensureDir(localPath)
         })
         .then(_ => {
+          console.log('SUCCESS! now fetching user details')
+        })
+        .then(_ => {
           console.log(`SUCCESS! created dir: ${localPath} `)
           return git(`${localPath}`)
             .silent(true)
+            .addConfig('user.name', this.FULL_NAME)
+            .addConfig('user.email', this.FULL_EMAIL)
             .clone(this.destinationAuth)
         })
         .then(_ => {
@@ -144,6 +151,14 @@ class Cloner {
           )
         })
         .catch(err => console.error('FAIL!! FAIL!! FAIL!!: ', err))
+        .then(_ => {
+          console.log('--> trying to remove this repo from our server')
+          return fs.remove(`${localPath}/${this.destinationRepo}`)
+        })
+        .then(() => {
+          console.log('--> successfully removed old repo folder!')
+          this.cloneLocal()
+        })
         .then(_ => {
           console.log('-- -- -- -- -- COMPLETE -- -- -- -- -- ')
         })
